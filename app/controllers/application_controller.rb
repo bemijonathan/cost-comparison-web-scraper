354ef67822a1794879161aa678f9e9b38c269292
lizzykate <bemijonathan@gmail.com>
2019-12-28 12:37:11 +0100

    API done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
-class ApplicationController < ActionController::Base
-  protect_from_forgery prepend: :true
+class ApplicationController < ActionController::API
 end
+
+
+# class ApplicationController < ActionController::Base
+#   protect_from_forgery prepend: :true
+# end
