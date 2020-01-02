c48db382f4fd1601305e0c6ce9d73f7716d873fd
bemijonathan <bemijonathan@gmail.com>
2020-01-02 02:06:28 +0100

    added page functionality

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,20 +5,11 @@ require 'open-uri'
 class HomeController < ApplicationController
 
     def index
-        render "home"
-    end
-    def term
-        @term = params[:term] 
-        puts "############"
-        #konga
-        @html = nogokiri(@term)
-        #jumia 
-        @jumia  = nokogiri_jumia(@term)
-        render "term"
+        render json: "home"
     end
 
-    def nogokiri(term)
-         @doc = Nokogiri::HTML(open("https://www.konga.com/search?search=#{term}",ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
+    def nogokiri_konga(term, page)
+         @doc = Nokogiri::HTML(open("https://www.konga.com/search?search=#{term}&page=#{page}", ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
         #  puts @doc
          item = @doc.search('li.bbe45_3oExY')
 
@@ -31,18 +22,20 @@ class HomeController < ApplicationController
                 :link => '',
                 :image => ""
             }
-            details[:image] = product.search('img.f5e10_VzEXF').attr('src')
+            details[:image] = product.search('img.f5e10_VzEXF')[0]['src']
             details[:name] = product.search('.af885_1iPzH').inner_text
             details[:price] = product.search('.d7c0f_sJAqi').inner_text
             details[:link] = product.search('._4941f_1HCZm a')[0]['href']
             items_to_render.push(details)
          end
 
-         return items_to_render
+         pages = @doc.search('._6d32f_2_vdu li:last-child').inner_text
+
+         return {items: items_to_render, page: pages}
     end 
     
-    def nokogiri_jumia(term)
-        @doc = Nokogiri::HTML(open("https://www.jumia.com.ng/catalog/?q=#{term}",ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
+    def nokogiri_jumia(term, page)
+        @doc = Nokogiri::HTML(open("https://www.jumia.com.ng/catalog/?q=#{term}&page=#{page}",ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE))
         item = @doc.search('div.sku.-gallery');
 
         puts item
@@ -57,12 +50,31 @@ class HomeController < ApplicationController
                 :image => ""
             }
 
-            details[:image] = product.search('img').attr('data-src')
+            details[:image] = product.css('.image-wrapper.default-state img').map{ |i| i['src'] } [1]
             details[:name] = product.search('.title .name').inner_text
             details[:price] = product.search('.price-box .price').inner_text
-            details[:link] = product.search('.sku.-gallery a').attr('href')
+            details[:link] = product.css('.link').map{ |i| i['href'] }[0]
+            # .map{ |i| i['href'] }
             items_to_render.push(details)
         end
-        return items_to_render
+
+        pages = @doc.search('section.pagination li:nth-last-child(2)').inner_text
+
+        return {items: items_to_render, page: pages}
     end 
+
+
+    def jumia 
+        @term = params[:term] 
+        @page = params[:page]
+        @jumia  = nokogiri_jumia(@term, @page)
+        render json: @jumia
+    end
+
+    def konga
+        @term = params[:term] 
+        @page = params[:page]
+        @konga = nogokiri_konga(@term, @page)
+        render json: @konga
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get '/' =>  "home#index"
+  # get '/' =>  "home#index"
   get '/term/:term' => "home#term"
+  get '/jumia' => "home#jumia"
+  get '/konga' => "home#konga"
 end
